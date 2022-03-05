5f7fb36de0472c0039d36ce6632fda16808c2c1d
1605296300@qq.com <long123456789>
2022-03-05 14:14:45 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Sensitivity_adapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(26, 15);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(64, 17);
+            this.label.TabIndex = 2;
+            this.label.Text = "输入X值：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "滚轮上开始重放";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(98, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 23);
+            this.textBox.TabIndex = 4;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 76);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sensitivity adapter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace Sensitivity_adapter
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        public IKeyboardMouseEvents mouseEvents;
+        public int RecX = 1400;
+        public Form1()
+        {
+            InitializeComponent();
+            mouseEvents = Hook.GlobalEvents();
+            mouseEvents.MouseWheel += OnMouseWeel;
+            textBox.Text = RecX.ToString();
+        }
+        public void OnMouseWeel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                mouse_event(0x0001, RecX, 0, 0, 0);
+            };
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                RecX = int.Parse(textBox.Text.ToString());
+            }
+            catch (Exception)
+            {
+                textBox.Text = "";
+            };
+            
+        }
+    }
+}
